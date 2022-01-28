85df8b68b808adb1608ec8a984ea3b33c4d4a310
aleksei demutski <aleksei.demutski@gmail.com>
2022-01-28 17:40:49 +0200

    upload

---
+++ 5th less/5-1.py
@@ -0,0 +1,8 @@
+x = 5
+student = {'name': 'Jack', 'age': 32, 'courses': ['Math', 'Art', 'Programming'],
+            0: 'int key', 0.2: 'float key', x: 'variable', 'variable': x}
+print(student[x])
+
+
+
+
