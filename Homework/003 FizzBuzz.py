8d08db1e3cc586d75fad2a65a8852205efa15e94
aleksei demutski <aleksei.demutski@gmail.com>
2022-01-31 15:54:58 +0200

    upload

--- Homework/003 FizzBuzz.py
+++ Homework/003 FizzBuzz.py
@@ -1,13 +1,14 @@
 num = range(1, 101)
 for number in num:
-    if number % 3 == 0:
-        print(number, 'Fizz')
-    if number % 5 == 0:
-        print(number, 'Buzz')
     if number % 3 and number % 5 == 0:
-        print(number, 'FizzBuzz')
+            print(number, 'FizzBuzz')
+    elif number % 3 == 0:
+        print(number, 'Fizz')
+    elif number % 5 == 0:
+        print(number, 'Buzz')
     else:
         print(number, False)
+
 
 
 
