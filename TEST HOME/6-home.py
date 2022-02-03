ef804c78db9915821dbc88d546ef6cd0db9a1f3d
aleksei demutski <aleksei.demutski@gmail.com>
2022-02-03 12:59:03 +0200

    upload

---
+++ TEST HOME/6-home.py
@@ -0,0 +1,12 @@
+list1 = [1, 2, 3, 4, 7, 9, 9]
+most_common = max(list1, key=list1.count)
+
+print(most_common)
+print(type(list1))
+
+list2 = [1, 2, 4, 6, 4, 6]
+most_common = max(list2, key=list2.count)
+
+print(most_common)
+print(type(list2))
+
