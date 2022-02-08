e0e0df7c27aee4d05d62bf60f86b379a6cda8af3
aleksei demutski <aleksei.demutski@gmail.com>
2022-02-08 16:09:19 +0200

    upload

---
+++ Homework/006 word_count.py
@@ -0,0 +1,28 @@
+with open('../../../PycharmProjects/Python7/007_working_with_files/text_files/trimushketera.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+text = text.replace('\n', ' ')
+text = text.replace(',', '').replace('.', '').replace('!', '').replace('?', '').replace(';', '').replace('"', '')
+text = text.lower()
+words = text.split()
+print('Количество слов в тексте: ',len(words))
+# print(type(words))
+
+uniq_words = set(words)
+print('Количество уникальных слов в тексте: ', len(uniq_words))
+
+with open('alpf_list.txt', 'w') as wfile:
+    alpf_words = list(uniq_words)
+    alpf_words.sort()
+    for wor in alpf_words:
+        print(wor)
+
+
+
+
+
+
+
+
+
+
+
